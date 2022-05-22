fb8142292b7f6e32ebf9466d5fe0840b8c1bf936
Nicky <fatemesahraee2000@gmail.com>
2022-05-22 13:09:37 +0430

    extend 48h :)

--- src/utils/timeLeft.js
+++ src/utils/timeLeft.js
@@ -1,5 +1,5 @@
 export const calculateTimeLeft = () => {
-    let countDownDate = new Date(2022, 5, 24, 6,0, 0).getTime();
+    let countDownDate = new Date(2022, 5, 23, 8,0, 0).getTime();
     let now = new Date().getTime();
     let distance = countDownDate - now;
 
